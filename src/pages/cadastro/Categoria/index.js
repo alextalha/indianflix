a29994ceac713e28f7ec9c9b47ce8bfabd6a062a
alextalha <Talha>
2020-08-01 14:30:55 -0300

    upgrade yarn

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,29 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault';
-import FormField from '../../../components/FormField';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PageDefault from "../../../components/PageDefault";
+import FormField from "../../../components/FormField";
 
 function CadastroCategoria() {
   const valoresIniciais = {
-    nome: '',
-    descricao: '',
-    cor: '',
-  }
+    nome: "",
+    descricao: "",
+    cor: "",
+  };
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
-
 
   function setValue(chave, valor) {
     // chave: nome, descricao, bla, bli
     setValues({
       ...values,
       [chave]: valor, // nome: 'valor'
-    })
+    });
   }
 
   function handleChange(infosDoEvento) {
     setValue(
-      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.getAttribute("name"),
       infosDoEvento.target.value
     );
   }
@@ -31,35 +30,33 @@ function CadastroCategoria() {
   // ============
 
   useEffect(() => {
-    if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
-      fetch(URL)
-       .then(async (respostaDoServer) =>{
-        if(respostaDoServer.ok) {
-          const resposta = await respostaDoServer.json();
-          setCategorias(resposta);
-          return; 
-        }
-        throw new Error('Não foi possível pegar os dados');
-       })
-    }    
+    const URL = window.location.href.includes("localhost")
+      ? "http://localhost:8080/categorias"
+      : "https://indianflix.herokuapp.com/categorias";
+
+    fetch(URL).then(async (respostaDoServer) => {
+      if (respostaDoServer.ok) {
+        const resposta = await respostaDoServer.json();
+        setCategorias(resposta);
+        return;
+      }
+      throw new Error("Não foi possível pegar os dados");
+    });
   }, []);
 
   return (
     <PageDefault>
       <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form onSubmit={function handleSubmit(infosDoEvento) {
+      <form
+        onSubmit={function handleSubmit(infosDoEvento) {
           infosDoEvento.preventDefault();
 
-          setCategorias([
-            ...categorias,
-            values
-          ]);
+          setCategorias([...categorias, values]);
 
-          setValues(valoresIniciais)
-      }}>
-
+          setValues(valoresIniciais);
+        }}
+      >
         <FormField
           label="Nome da Categoria"
           type="text"
@@ -106,27 +103,18 @@ function CadastroCategoria() {
           </label>
         </div> */}
 
-        <button>
-          Cadastrar
-        </button>
+        <button>Cadastrar</button>
       </form>
-      
 
       <ul>
         {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria.titulo}
-            </li>
-          )
+          return <li key={`${categoria}${indice}`}>{categoria.titulo}</li>;
         })}
       </ul>
 
-      <Link to="/">
-        Ir para home
-      </Link>
+      <Link to="/">Ir para home</Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
