0eddf1f0015f88bfb4f1c08f5f46d106de978b2a
alextalha <Talha>
2020-08-01 12:46:54 -0300

    config do fake server e outros modulos

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
 const FormFieldWrapper = styled.div`
   position: relative;
   textarea {
     min-height: 150px;
   }
-
   input[type="color"] {
     padding-left: 56px;
   }
@@ -16,65 +15,65 @@ const FormFieldWrapper = styled.div`
 const Label = styled.label``;
 
 Label.Text = styled.span`
-  color: #E5E5E5;
+  color: #e5e5e5;
   height: 57px;
-  position: absolute; 
+  position: absolute;
   top: 0;
   left: 16px;
-  
+
   display: flex;
   align-items: center;
-  
+
   transform-origin: 0% 0%;
   font-size: 18px;
   font-style: normal;
-  font-weight: 300;  
-  transition: .1s ease-in-out;
+  font-weight: 300;
+
+  transition: 0.1s ease-in-out;
 `;
 
 const Input = styled.input`
-  background: #53585D;
-  color: #F5F5F5;
+  background: #53585d;
+  color: #f5f5f5;
   display: block;
   width: 100%;
   height: 57px;
   font-size: 18px;
-  
+
   outline: 0;
   border: 0;
   border-top: 4px solid transparent;
-  border-bottom: 4px solid #53585D;
-  
+  border-bottom: 4px solid #53585d;
+
   padding: 16px 16px;
   margin-bottom: 45px;
-  
+
   resize: none;
   border-radius: 4px;
-  transition: border-color .3s;
-  
+  transition: border-color 0.3s;
+
   &:focus {
     border-bottom-color: var(--primary);
   }
-  &:focus:not([type='color']) + ${Label.Text} {
-    transform: scale(.6) translateY(-10px);
+  &:focus:not([type="color"]) + ${Label.Text} {
+    transform: scale(0.6) translateY(-10px);
   }
-  
   ${({ value }) => {
     const hasValue = value.length > 0;
-    return hasValue && css`
-        &:not([type='color']) + ${Label.Text} {
-          transform: scale(.6) translateY(-10px);
+    return (
+      hasValue &&
+      css`
+        &:not([type="color"]) + ${Label.Text} {
+          transform: scale(0.6) translateY(-10px);
         }
-      `;
-  }
-}
+      `
+    );
+  }}
 `;
 
-function FormField({
-  label, type, name, value, onChange,
-}) {
-  const isTypeTextArea = type === 'textarea';
-  const tag = isTypeTextArea ? 'textarea' : 'input';
+function FormField({ label, type, name, value, onChange }) {
+  const isTypeTextArea = type === "textarea";
+  const tag = isTypeTextArea ? "textarea" : "input";
 
   return (
     <FormFieldWrapper>
@@ -86,18 +85,15 @@ function FormField({
           name={name}
           onChange={onChange}
         />
-        <Label.Text>
-          {label}
-          :
-        </Label.Text>
+        <Label.Text>{label}:</Label.Text>
       </Label>
     </FormFieldWrapper>
   );
 }
 
 FormField.defaultProps = {
-  type: 'text',
-  value: '',
+  type: "text",
+  value: "",
 };
 
 FormField.propTypes = {
