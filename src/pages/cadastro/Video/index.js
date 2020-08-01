5f9a86ef44ebaad356a71bbe2cc51bafc2e7f052
alextalha <Talha>
2020-08-01 14:05:23 -0300

    configuração no arquivo de video

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,18 +1,18 @@
 import React, { Fragment } from "react";
 
+import { Link } from "react-router-dom";
 import Menu from "../../../components/Menu/index";
 import Footer from "../../../components/Footer/index";
 import PageDefault from "../../../components/PageDefault";
-import { Link } from "react-router-dom";
 
 function CadastroVideo() {
   return (
-    <Fragment>
+    <>
       <PageDefault>
         <h1>Cadastro de Vídeo </h1>
         <Link to="/cadastro/categoria">Cadastrar Categoria</Link>
       </PageDefault>
-    </Fragment>
+    </>
   );
 }
 
